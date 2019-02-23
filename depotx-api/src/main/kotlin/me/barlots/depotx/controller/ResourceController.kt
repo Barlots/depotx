b8cadeffd48cfca0c7df91740b7d238351732abc
Barlots <barlots1@gmail.com>
2019-02-23 16:20:28 +0100

    added image controller nad request return dto

--- depotx-api/src/main/kotlin/me/barlots/depotx/controller/ResourceController.kt
+++ depotx-api/src/main/kotlin/me/barlots/depotx/controller/ResourceController.kt
@@ -1,6 +1,7 @@
 package me.barlots.depotx.controller
 
 import me.barlots.depotx.entity.User
+import me.barlots.depotx.entity.dto.ImageLinkDto
 import me.barlots.depotx.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
@@ -17,5 +18,12 @@ class ResourceController {
     fun addString(@PathVariable xxx: String): ResponseEntity<Any> {
         userRepository.save(User().apply { content = xxx })
         return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @PostMapping("/images")
+    fun addImage(@RequestParam filename: String): ResponseEntity<Any> {
+        //userRepository.save(User().apply { content = xxx })
+        val link = ImageLinkDto(filename, filename)
+        return ResponseEntity(link, HttpStatus.CREATED)
     }
 }
---
+++ depotx-api/src/main/kotlin/me/barlots/depotx/entity/dto/ImageLinkDto.kt
@@ -0,0 +1,6 @@
+package me.barlots.depotx.entity.dto
+
+public class ImageLinkDto(resourceUrl: String, deleteUrl: String) {
+    var ResourceUrl: String = resourceUrl
+    var DeleteUrl: String = deleteUrl
+}
