87770ec72b39a2ac320e8037a4afd3c2182b545f
Bartosz Lukowicz <barlots1@gmail.com>
2019-03-05 19:58:06 +0100

    add test functionality of image resizing

--- depotx-api/src/main/kotlin/me/barlots/depotx/controller/ResourceController.kt
+++ depotx-api/src/main/kotlin/me/barlots/depotx/controller/ResourceController.kt
@@ -1,36 +1,27 @@
 package me.barlots.depotx.controller
 
-import me.barlots.depotx.entity.dto.ImageLinkDto
 import me.barlots.depotx.service.FileService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.ui.ModelMap
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.multipart.MultipartFile
 
 @Controller
 class ResourceController {
-//    @Autowired
-//    lateinit var userRepository: UserRepository
 
     @Autowired
     lateinit var fileService: FileService
 
-//    @PostMapping("/strings/{xxx}")
-//    fun addString(@PathVariable xxx: String): ResponseEntity<Any> {
-//        userRepository.save(User().apply { content = xxx })
-//        return ResponseEntity(HttpStatus.CREATED)
-//    }
-
     @PostMapping("/images")
     fun uploadImage(@RequestParam file: MultipartFile): ResponseEntity<Any> {
-        return ResponseEntity("FileSize: ${file.size}; FileName ${file.originalFilename}", HttpStatus.CREATED)
+        return ResponseEntity("${fileService.handleImage(file)}", HttpStatus.CREATED)
     }
 
     @PostMapping("/services")
-    fun testService(@RequestParam sentence: String) : ResponseEntity<Any> {
+    fun testService(@RequestParam sentence: String): ResponseEntity<Any> {
         val said = fileService.say(sentence);
         return ResponseEntity(said, HttpStatus.OK)
     }
--- depotx-api/src/main/kotlin/me/barlots/depotx/service/FileService.kt
+++ depotx-api/src/main/kotlin/me/barlots/depotx/service/FileService.kt
@@ -3,6 +3,10 @@ package me.barlots.depotx.service
 import me.barlots.depotx.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import java.awt.Image
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
 
 @Service
 class FileService {
@@ -11,4 +15,33 @@ class FileService {
     lateinit var userRepository: UserRepository
 
     fun say(msg: String) = "Service said: $msg"
+
+    //temporary functionality
+    fun handleImage(multipart: MultipartFile): String {
+        var extension = getExtension(multipart.name);
+
+        if (extension != ".png" && extension != ".jpg") {
+            throw UnsupportedOperationException("Unsupported extension: $extension")
+        }
+
+        var fullsize = ImageIO.read(multipart.inputStream)
+        var thumbnail = generateThumbnail(fullsize);
+
+        var sizeRatio = thumbnail.getHeight(null) * 100 * thumbnail.getWidth(null) / fullsize.getHeight(null) / fullsize.getWidth(null)
+
+        return "thumnail is $sizeRatio% size of original image"
+    }
+
+    private fun generateThumbnail(fullsize: BufferedImage): Image {
+        var tmp = fullsize.getScaledInstance(100, 100, Image.SCALE_SMOOTH)
+        var thumbnail = BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB)
+
+        var g2d = thumbnail.createGraphics()
+        g2d.drawImage(tmp, 0, 0, null)
+        g2d.dispose()
+
+        return thumbnail
+    }
+
+    private fun getExtension(filename: String) = filename.substring(filename.lastIndexOf('.') + 1)
 }
